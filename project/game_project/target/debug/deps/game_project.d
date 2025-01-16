9fe5d96520194c09922e40e3adce18281cb60164
pchyuk <pch6590@gmail.com>
2025-01-16 09:49:15 +0900

    게임 전적 관리 프로젝트 수정

--- project/game_project/target/debug/deps/game_project.d
+++ project/game_project/target/debug/deps/game_project.d
@@ -1,6 +1,6 @@
-C:\Users\user\Desktop\Rust_study\book\game_project\target\debug\deps\game_project.exe: src/main.rs src\game.rs src\user.rs src\utils.rs
+C:\Users\user\Desktop\Rust_study\project\game_project\target\debug\deps\game_project.exe: src/main.rs src\game.rs src\user.rs src\utils.rs
 
-C:\Users\user\Desktop\Rust_study\book\game_project\target\debug\deps\game_project.d: src/main.rs src\game.rs src\user.rs src\utils.rs
+C:\Users\user\Desktop\Rust_study\project\game_project\target\debug\deps\game_project.d: src/main.rs src\game.rs src\user.rs src\utils.rs
 
 src/main.rs:
 src\game.rs:
